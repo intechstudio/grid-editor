83420e9a7d1e0a2023b8ebb6ef4e970d88e0d722
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2020-05-11 11:35:19 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<Tailwindcss />
+<h1>Hello {name}!</h1>
