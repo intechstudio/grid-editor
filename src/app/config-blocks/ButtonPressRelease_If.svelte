9d69a2290384f5779fd0b4d5d9d3736a5747b36f
sukuwc <suku@intech.studio>
2022-06-17 13:13:55 +0200

    SUKU Press/Release modblock added ED-66

---
+++ src/app/config-blocks/ButtonPressRelease_If.svelte
@@ -0,0 +1,110 @@
+<script context="module">
+  // config descriptor parameters
+  export const information = {
+    short: 'bpr',
+    name: 'ButtonPressRelease_If',
+    rendering: 'modifier',
+    rounding: 'top',
+    category: 'special',
+    eventtype: [3], // 2: encoder
+    desc: 'Press/Release',
+    blockTitle: 'Press',
+    defaultLua: 'if self:bst()>0 then--[[@bprel]] else--[[@bpre]] end',
+    icon: `
+    <svg width="100%" height="100%" viewBox="0 0 445 327" fill="none" xmlns="http://www.w3.org/2000/svg">
+   
+      <rect
+     style="fill:#000000;fill-rule:evenodd;stroke-width:0.809819"
+     id="rect365-3"
+     width="203.78975"
+     height="35.209686"
+     x="120.60513"
+     y="-337.43585"
+     transform="scale(1,-1)" />
+  <rect
+     style="fill:#000000;fill-rule:evenodd;stroke-width:0.803988"
+     id="rect365"
+     width="37.381088"
+     height="189.19719"
+     x="203.80945"
+     y="1.8109859" />
+  <path
+     sodipodi:type="star"
+     style="fill:#000000;fill-rule:evenodd"
+     id="path653"
+     inkscape:flatsided="false"
+     sodipodi:sides="3"
+     sodipodi:cx="222.688"
+     sodipodi:cy="253.84973"
+     sodipodi:r1="83.359268"
+     sodipodi:r2="41.679634"
+     sodipodi:arg1="-0.52359878"
+     sodipodi:arg2="0.52359878"
+     inkscape:rounded="0"
+     inkscape:randomized="0"
+     inkscape:transform-center-y="20.839817"
+     transform="translate(-0.18800336,-64.43105)"
+     d="M 294.87925,212.1701 258.78363,274.68955 222.688,337.209 186.59238,274.68955 150.49676,212.1701 l 72.19124,0 z" />
+    </svg>
+    `,
+    color: '#4A4AA7 '
+  }
+</script>
+
+<script>
+  import { createEventDispatcher, onDestroy } from 'svelte';
+  import stringManipulation from '../main/user-interface/_string-operations';
+  import { parenthesis } from './_validators';
+
+  export let config = ''
+  export let index;
+
+  export let access_tree;
+
+  import LineEditor from '../main/user-interface/LineEditor.svelte'
+
+  let sidebarWidth;
+
+  const dispatch = createEventDispatcher();
+
+  let scriptSegment = ''; // local script part
+
+  let loaded = false;
+
+  $: if(config.script && !loaded){
+    scriptSegment = stringManipulation.humanize(config.script.slice(3, -5));
+    loaded = true;
+  }
+
+  onDestroy(()=>{
+    loaded = false;
+  })
+
+  function sendData(e){
+    if(parenthesis(e)){
+      const script = stringManipulation.shortify(e);
+      dispatch('output', {short: information.short, script: information.defaultLua})
+    }
+  }
+
+</script>
+
+
+<svelte:window bind:innerWidth={sidebarWidth} />
+
+<if-block class="w-full h-fit flex flex-col text-white py-1 {information.rounding == 'top'?"rounded-tl-2xl ":""} {information.rounding == 'bottom'?"rounded-bl-2xl ":""} " style="min-height: 2.5rem; background: {information.color};">
+  
+
+
+
+  <div class="bg-secondary p-1 my-auto mr-1 rounded hidden" >
+    <LineEditor
+      on:output={(e)=>{sendData(e.detail.script)}}
+      {access_tree}
+      {sidebarWidth}
+      value={scriptSegment}
+    />
+  </div>
+
+
+</if-block>
