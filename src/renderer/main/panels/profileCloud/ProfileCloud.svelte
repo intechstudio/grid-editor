e660248b898b356db8cd654a90e86bb76c012cee
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2023-05-09 00:14:19 +0200

    KKERTI grind interface for profile cloud

--- src/renderer/main/panels/profileCloud/ProfileCloud.svelte
+++ src/renderer/main/panels/profileCloud/ProfileCloud.svelte
@@ -1,20 +1,38 @@
 <script>
   import { onDestroy, onMount } from "svelte";
+  import { v4 as uuidv4 } from "uuid";
   import { appSettings } from "../../../runtime/app-helper.store";
 
   import { get } from "svelte/store";
   const { env } = window.ctxProcess;
 
-  import configuration from "../../../../../configuration.json";
-  import { logger } from "../../../runtime/runtime.store";
+  import {
+    engine,
+    logger,
+    runtime,
+    user_input,
+  } from "../../../runtime/runtime.store";
 
   import { authStore } from "$lib/auth.store"; // this only changes if login, logout happens
   import { userStore } from "$lib/user.store";
+  import { selectedProfileStore } from "../../../runtime/profile-helper.store";
 
   let iframe_element;
-  let iframe_response_element;
 
   $: sendAuthEventToIframe($authStore);
+
+  let selectedModule = undefined;
+
+  $: {
+    const ui = $user_input;
+    let device = get(runtime).find(
+      (device) => device.dx == ui.brc.dx && device.dy == ui.brc.dy
+    );
+
+    if (typeof device !== "undefined") {
+      selectedModule = device.id.substr(0, 4);
+    }
+  }
 
   function sendAuthEventToIframe(authEvent) {
     if (iframe_element == undefined) return;
@@ -33,19 +51,22 @@
     );
   }
 
-  async function handleMessageReceive(event) {
+  async function handleImportProfile(event) {
     if (event.data.channelMessageType == "IMPORT_PROFILE") {
       const path = $appSettings.persistant.profileFolder;
+      // owner is not used, but user instead
       const { owner, name, editorData, _id } = event.data;
       const profile = JSON.parse(editorData);
       profile.id = _id; // this should be _id instead of id!
+      profile.name = uuidv4();
       await window.electron.configs
-        .saveConfig(path, name, profile, "profiles", owner)
+        .saveConfig(path, profile.name, profile, "profiles", "local")
         .then((res) => {
           logger.set({
             type: "success",
             message: `Profile ${name} imported successfully`,
           });
+          console.log(channel);
           channel.postMessage({ ok: true, data: {} });
         })
         .catch((err) => {
@@ -58,12 +79,171 @@
     }
   }
 
-  let channel; // communication channel received from iframe, used for profile import responses
+  async function handleGetListOfLocalProfiles(event) {
+    if (event.data.channelMessageType == "GET_LIST_OF_LOCAL_PROFILES") {
+      const path = $appSettings.persistant.profileFolder;
+      const profiles = await window.electron.configs.loadConfigsFromDirectory(
+        path,
+        "profiles"
+      );
+      console.log(profiles);
+      channel.postMessage({ ok: true, data: profiles });
+    }
+  }
+
+  async function handleProvideSelectedProfileForOptionalUploadingToOneOreMoreModules(
+    event
+  ) {
+    if (
+      event.data.channelMessageType ==
+      "PROVIDE_SELECTED_PROFILE_FOR_OPTIONAL_UPLOADING_TO_ONE_OR_MORE_MODULES"
+    ) {
+      selectedProfileStore.set(event.data.profile);
+      channel.postMessage({ ok: true, data: "" });
+    }
+  }
+
+  async function handleDeleteLocalProfile(event) {
+    if (event.data.channelMessageType == "DELETE_LOCAL_PROFILE") {
+      const path = $appSettings.persistant.profileFolder;
+      const { folder, name } = event.data?.profile;
+      await window.electron.configs
+        .deleteConfig(path, name, "profiles", folder)
+        .then((res) => {
+          logger.set({
+            type: "success",
+            message: `Profile ${name} deleted successfully`,
+          });
+          channel.postMessage({ ok: true, data: {} });
+        })
+        .catch((err) => {
+          logger.set({
+            type: "fail",
+            message: `Profile ${name} deletion failed`,
+          });
+          channel.postMessage({ ok: false, data: {} });
+        });
+    }
+  }
+
+  async function handleCreateNewLocalProfileWithTheSelectedModulesConfigurationFromEditor(
+    event
+  ) {
+    if (
+      event.data.channelMessageType ==
+      "CREATE_NEW_LOCAL_PROFILE_WITH_THE_SELECTED_MODULES_CONFIGURATION_FROM_EDITOR"
+    ) {
+      const path = $appSettings.persistant.profileFolder;
+      // this would be the needed data, if the profile would come from the profile cloud (profile.editorData...)
+      // const { owner, name, editorData, _id } = event.data;
+
+      window.electron.analytics.influx("profile-library", {
+        value: "newProfile_add_to_session",
+      });
+      window.electron.analytics.influx(
+        "application",
+        "profiles",
+        "profile",
+        "newProfile_add_to_session"
+      );
+
+      let callback = await async function () {
+        logger.set({
+          type: "progress",
+          mode: 0,
+          classname: "profilesave",
+          message: `Ready to save profile!`,
+        });
+
+        const li = get(user_input);
+
+        const configs = get(runtime);
+
+        let name = `${uuidv4()}`;
+        let description = "";
+        let type = selectedModule;
+
+        let profile = {
+          name: name,
+          description: description,
+          type: type,
+          isGridProfile: true, // differentiator from different JSON files!
+          version: {
+            major: $appSettings.version.major,
+            minor: $appSettings.version.minor,
+            patch: $appSettings.version.patch,
+          },
+          id: uuidv4(),
+        };
+
+        configs.forEach((d) => {
+          if (d.dx == li.brc.dx && d.dy == li.brc.dy) {
+            const page = d.pages.find(
+              (x) => x.pageNumber == li.event.pagenumber
+            );
+
+            profile.configs = page.control_elements.map((cfg) => {
+              return {
+                controlElementNumber: cfg.controlElementNumber,
+                events: cfg.events.map((ev) => {
+                  return {
+                    event: ev.event.value,
+                    config: ev.config,
+                  };
+                }),
+              };
+            });
+          }
+        });
+
+        await window.electron.configs.saveConfig(
+          path,
+          name,
+          profile,
+          "profiles",
+          "local"
+        );
+
+        logger.set({
+          type: "success",
+          message: `Profile saved!`,
+        });
+
+        engine.set("ENABLED");
+
+        channel.postMessage({ ok: true, data: {} });
+      };
+
+      runtime.fetch_page_configuration_from_grid(callback);
+    }
+  }
+
+  let channel; // communication channel received from iframe
   function initChannelCommunication(event) {
-    if (event.data == "profileImportCommunication") {
-      if (event.ports && event.ports.length) {
-        channel = event.ports[0];
-        channel.onmessage = handleMessageReceive;
+    if (event.ports && event.ports.length) {
+      channel = event.ports[0];
+      if (event.data == "profileImportCommunication") {
+        channel.onmessage = handleImportProfile;
+      }
+      if (event.data == "getListOfLocalProfiles") {
+        channel.onmessage = handleGetListOfLocalProfiles;
+      }
+      if (
+        event.data ==
+        "provideSelectedProfileForOptionalUploadingToOneOreMoreModules"
+      ) {
+        channel.onmessage =
+          handleProvideSelectedProfileForOptionalUploadingToOneOreMoreModules;
+      }
+      if (event.data == "deleteLocalProfile") {
+        channel.onmessage = handleDeleteLocalProfile;
+      }
+      if (
+        event.data ==
+        "createNewLocalProfileWithTheSelectedModulesConfigurationFromEditor"
+      ) {
+        channel.onmessage =
+          handleCreateNewLocalProfileWithTheSelectedModulesConfigurationFromEditor;
       }
     }
   }
