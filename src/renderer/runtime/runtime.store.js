fb30dd4da3a79c6964514683a4860ac8c6d739e5
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2023-06-02 13:50:19 +0200

    KKERTI prettier

--- src/renderer/runtime/runtime.store.js
+++ src/renderer/runtime/runtime.store.js
@@ -108,7 +108,7 @@ export function update_elementPositionStore(descr) {
   }
   if (
     eps[descr.brc_parameters.SX][descr.brc_parameters.SY][
-    descr.class_parameters.ELEMENTNUMBER
+      descr.class_parameters.ELEMENTNUMBER
     ] === undefined
   ) {
     eps[descr.brc_parameters.SX][descr.brc_parameters.SY][
@@ -134,7 +134,7 @@ export function update_elementNameStore(descr) {
   }
   if (
     ens[descr.brc_parameters.SX][descr.brc_parameters.SY][
-    descr.class_parameters.NUM
+      descr.class_parameters.NUM
     ] === undefined
   ) {
     ens[descr.brc_parameters.SX][descr.brc_parameters.SY][
@@ -162,13 +162,13 @@ export function update_elementPositionStore_fromPreview(descr) {
   for (let i = 1; i < descr.class_parameters.LENGTH / 4; i++) {
     const num = parseInt(
       "0x" +
-      String.fromCharCode(descr.raw[4 + i * 4 + 0]) +
-      String.fromCharCode(descr.raw[4 + i * 4 + 1])
+        String.fromCharCode(descr.raw[4 + i * 4 + 0]) +
+        String.fromCharCode(descr.raw[4 + i * 4 + 1])
     );
     const val = parseInt(
       "0x" +
-      String.fromCharCode(descr.raw[4 + i * 4 + 2]) +
-      String.fromCharCode(descr.raw[4 + i * 4 + 3])
+        String.fromCharCode(descr.raw[4 + i * 4 + 2]) +
+        String.fromCharCode(descr.raw[4 + i * 4 + 3])
     );
     //console.log(num, val)
 
@@ -188,23 +188,23 @@ export function update_ledColorStore(descr) {
   for (let i = 0; i < descr.class_parameters.LENGTH / 8; i++) {
     const num = parseInt(
       "0x" +
-      String.fromCharCode(descr.raw[8 + i * 8 + 0]) +
-      String.fromCharCode(descr.raw[8 + i * 8 + 1])
+        String.fromCharCode(descr.raw[8 + i * 8 + 0]) +
+        String.fromCharCode(descr.raw[8 + i * 8 + 1])
     );
     const red = parseInt(
       "0x" +
-      String.fromCharCode(descr.raw[8 + i * 8 + 2]) +
-      String.fromCharCode(descr.raw[8 + i * 8 + 3])
+        String.fromCharCode(descr.raw[8 + i * 8 + 2]) +
+        String.fromCharCode(descr.raw[8 + i * 8 + 3])
     );
     const gre = parseInt(
       "0x" +
-      String.fromCharCode(descr.raw[8 + i * 8 + 4]) +
-      String.fromCharCode(descr.raw[8 + i * 8 + 5])
+        String.fromCharCode(descr.raw[8 + i * 8 + 4]) +
+        String.fromCharCode(descr.raw[8 + i * 8 + 5])
     );
     const blu = parseInt(
       "0x" +
-      String.fromCharCode(descr.raw[8 + i * 8 + 6]) +
-      String.fromCharCode(descr.raw[8 + i * 8 + 7])
+        String.fromCharCode(descr.raw[8 + i * 8 + 6]) +
+        String.fromCharCode(descr.raw[8 + i * 8 + 7])
     );
 
     //console.log(num, red, gre, blu)
@@ -367,7 +367,7 @@ function create_user_input() {
 
         let elementtype =
           grid.moduleElements[device.id.split("_")[0]][
-          store.event.elementnumber
+            store.event.elementnumber
           ];
         store.event.elementtype = elementtype;
 
@@ -1207,7 +1207,7 @@ function create_runtime() {
           setDefaultSelectedElement(rt[0]);
         }
       }
-    } catch (error) { }
+    } catch (error) {}
 
     mixpanel.track("Disconnect Module", {
       action: "Disconnect",
