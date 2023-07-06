55633bbb2da256b605cb0a0f778f77b288368656
elsoazemelet <elsoazemelet@gmail.com>
2023-07-06 15:00:02 +0200

    Format

--- src/renderer/serialport/instructions.js
+++ src/renderer/serialport/instructions.js
@@ -94,7 +94,7 @@ const instructions = {
           element,
           event,
           actionstring,
-          "GRID_REPORT"
+          "GRID_REPORT",
         );
 
         if (callback) {
@@ -122,7 +122,7 @@ const instructions = {
     }
 
     const objIndex = get(unsaved_changes).findIndex(
-      (e) => e.x == dx && e.y == dy
+      (e) => e.x == dx && e.y == dy,
     );
     if (objIndex !== -1) {
       unsaved_changes.update((s) => {
