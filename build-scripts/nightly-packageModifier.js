c5475a96fcf95f48a7bb0b3a1d38f005a594f8f3
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2023-08-03 11:21:07 +0200

    KKERTI test package modifier nightly

---
+++ build-scripts/nightly-packageModifier.js
@@ -0,0 +1,18 @@
+// File: build-scripts/updatePackageJson.js
+
+const fs = require('fs');
+const path = require('path');
+
+let packageJsonPath = path.join(__dirname, '../package.json');
+let packageJson = require(packageJsonPath);
+
+let branchName = process.env.BRANCH_NAME;
+let releaseChannel = process.env.RELEASE_CHANNEL;
+
+packageJson.build.productName += `(Nightly) -${branchName}`;
+packageJson.version += `-${releaseChannel}`;
+
+fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+    if (err) throw err;
+    console.log('package.json has been updated!');
+});
