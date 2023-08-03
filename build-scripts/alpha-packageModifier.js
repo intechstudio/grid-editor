049358e2ef72950f4b292e61b4cd27a2b4fd8e88
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2023-08-03 13:58:34 +0200

    KKERTI test changes

--- build-scripts/alpha-packageModifier.js
+++ build-scripts/alpha-packageModifier.js
@@ -7,8 +7,10 @@ let packageJsonPath = path.join(__dirname, "../package.json");
 let packageJson = require(packageJsonPath);
 
 let branchName = process.env.BRANCH_NAME;
+let releaseVersion = process.env.RELEASE_VERSION;
 
-packageJson.build.productName += ` (Nightly) ${branchName}`;
+packageJson.build.productName += ` (Alpha)`;
+packageJson.version = `${releaseVersion}`;
 
 fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
   if (err) throw err;
