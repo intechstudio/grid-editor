d1c41950c62d28e94947d36b2322142ee2c08d21
Pásztor Dániel <danim1130@gmail.com>
2023-09-08 15:07:30 +0200

    Flatten configs directory

--- src/electron/src/profiles.ts
+++ src/electron/src/profiles.ts
@@ -146,69 +146,37 @@ export async function loadConfigsFromDirectory(configPath, rootDirectory) {
   if (!fs.existsSync(`${path}/${rootDirectory}`))
     fs.mkdirSync(`${path}/${rootDirectory}`);
 
-  // either presets or profiles (pages)
-  // make sure to figure out naming conventions!,
   let configs = [];
 
   const [stats] = await checkIfWritableDirectory(`${path}/${rootDirectory}`);
 
   if (stats.isDirectory) {
-    // get list of directories
-    let dirs = fs
-      .readdirSync(`${path}/${rootDirectory}`, { withFileTypes: true })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name);
+    const files = await fs.promises.readdir(
+      `${path}/${rootDirectory}`,
+    );
 
-    // for all directories...
+    for (const file of files) {
+      let filepath =
+        path +
+        "/" +
+        rootDirectory +
+        "/" +
+        file;
 
-    for (const dir of dirs) {
-      const directories = await fs.promises.readdir(
-        `${path}/${rootDirectory}/${dir}`,
-      );
-      for (const directory of directories) {
-        let directorypath =
-          path + "/" + rootDirectory + "/" + dir + "/" + directory;
-        const [stats] = await checkIfWritableDirectory(directorypath);
-        if (stats.isDirectory) {
-          const files = await fs.promises.readdir(
-            `${path}/${rootDirectory}/${dir}/${directory}`,
-          );
-          for (const file of files) {
-            let filepath =
-              path +
-              "/" +
-              rootDirectory +
-              "/" +
-              dir +
-              "/" +
-              directory +
-              "/" +
-              file;
-            const [stats] = await checkIfWritableDirectory(directorypath);
-
-            await fs.promises.readFile(filepath, "utf-8").then(async (data) => {
-              if (isJson(data)) {
-                let obj = JSON.parse(data);
-                if (obj.isGridProfile || obj.isGridPreset || obj.configType) {
-                  obj.folder = dir;
-                  obj.showMore = false;
-                  obj.color = stringToColor(dir);
-                  const dateObject = await getDateOfModify(filepath);
-                  obj.fsCreatedAt = dateObject.createdAt;
-                  obj.fsModifiedAt = dateObject.modifiedAt;
-                  configs.push(obj);
-                } else {
-                  log.info("JSON is not a grid config!");
-                }
-              } else {
-                log.info("Not a file!");
-              }
-            });
+      await fs.promises.readFile(filepath, "utf-8").then(async (data) => {
+        if (isJson(data)) {
+          let obj = JSON.parse(data);
+          if (obj.configType) {
+            const dateObject = await getDateOfModify(filepath);
+            obj.fsModifiedAt = dateObject.modifiedAt;
+            configs.push(obj);
+          } else {
+            log.info("JSON is not a grid config!");
           }
         } else {
-          log.info("Not a directory!");
+          log.info("Not a file!");
         }
-      }
+      });
     }
   } else {
     log.info("Not a directory!");
@@ -319,38 +287,4 @@ async function getDateOfModify(filepath) {
     .catch((err) => console.error("get date modify", err));
 
   return dateObject;
-}
-
-function stringToColor(string) {
-  // Generte Hash
-
-  var hash = 0;
-  if (string.length == 0) return hash;
-  for (let i = 0; i < string.length; i++) {
-    var charCode = string.charCodeAt(i);
-    hash = (hash << 7) - hash + charCode;
-    hash = hash & hash;
-  }
-
-  // define the color params
-
-  var hue = Math.abs(hash) % 360; // degrees
-  var sat = 65; // percentage
-  var lum = 50; // percentage
-
-  // convert from HSL to RGB
-
-  lum /= 100;
-  const a = (sat * Math.min(lum, 1 - lum)) / 100;
-  const f = (n) => {
-    const k = (n + hue / 30) % 12;
-    const col = lum - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-    return Math.round(255 * col)
-      .toString(16)
-      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
-  };
-
-  var color = `#${f(0)}${f(8)}${f(4)}`;
-
-  return color;
 }
