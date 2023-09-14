a53bc786f26d6820ddbbae95a1d2484d4d422f60
elsoazemelet <elsoazemelet@gmail.com>
2023-09-14 16:48:41 +0200

    Increased size

--- src/electron/main-store.ts
+++ src/electron/main-store.ts
@@ -3,8 +3,8 @@ import Store from "electron-store";
 export const store = new Store({
   defaults: {
     windowBounds: {
-      width: 1280,
-      height: 800,
+      width: 1366,
+      height: 768,
     },
     profiles_folder: "",
   },
