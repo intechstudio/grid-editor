894c80e14c0c65a87f3f476d5d1109baae0d81d3
suku <sukuwc@riseup.net>
2023-09-27 09:28:25 +0200

    SUKU node minimum versiuon required preinstall logic added, editor version bump to 45

---
+++ build-scripts/check-node-version.js
@@ -0,0 +1,10 @@
+const semver = require('semver');
+const packageJson = require('../package.json');
+
+const requiredNodeVersion = packageJson.engines.node;
+const currentNodeVersion = process.version;
+
+if (!semver.satisfies(currentNodeVersion, requiredNodeVersion)) {
+  console.error(`Required Node.js version ${requiredNodeVersion} not satisfied by current version ${currentNodeVersion}.`);
+  process.exit(1); // Exit the installation process with an error code
+}
