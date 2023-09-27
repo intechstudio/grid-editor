4e0a53da45a814525293657525f0e39f14a006b0
suku <sukuwc@riseup.net>
2023-09-27 15:01:32 +0200

    SUKU builder references updated for external files

--- build-scripts/alpha-packageModifier.js
+++ build-scripts/alpha-packageModifier.js
@@ -3,15 +3,33 @@
 const fs = require("fs");
 const path = require("path");
 
+// Update package version in package.json
+
 let packageJsonPath = path.join(__dirname, "../package.json");
 let packageJson = require(packageJsonPath);
 
-let releaseVersion = process.env.RELEASE_VERSION;
-
-packageJson.build.productName += ` (Alpha)`;
-packageJson.version = `${releaseVersion}`;
+packageJson.version = `${process.env.RELEASE_VERSION}`;
 
 fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
   if (err) throw err;
   console.log("package.json has been updated!");
 });
+
+// Update product name in electron-builder.json
+
+let electronBuilderConfigJsonPath = path.join(
+  __dirname,
+  "../electron-builder.json"
+);
+let electronBuilderConfigJson = require(electronBuilderConfigJsonPath);
+
+electronBuilderConfigJson.productName += ` (Alpha)`;
+
+fs.writeFile(
+  electronBuilderConfigJsonPath,
+  JSON.stringify(electronBuilderConfigJson, null, 2),
+  (err) => {
+    if (err) throw err;
+    console.log("electron-builder.json has been updated!");
+  }
+);
