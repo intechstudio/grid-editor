2a8c2f118a9fb56091f61754f81f4064df1f42f7
elsoazemelet <elsoazemelet@gmail.com>
2023-10-12 11:40:25 +0200

    Added missing files

---
+++ src/renderer/main/panels/configuration/components/AddActionButton.svelte
@@ -0,0 +1,42 @@
+<script>
+  import ActionPicker from "./ActionPicker.svelte";
+  import { createEventDispatcher } from "svelte";
+
+  export let index = undefined;
+  let showActionPicker = false;
+  let referenceElement = undefined;
+
+  const dispatch = createEventDispatcher();
+
+  function handleNewConfig(e) {
+    dispatch("new-config", e.detail);
+  }
+
+  function handleShowActionPicker() {
+    showActionPicker = true;
+  }
+
+  function handleCloseActionPicker() {
+    showActionPicker = false;
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<action-placeholder
+  bind:this={referenceElement}
+  on:click={handleShowActionPicker}
+  on:new-config={handleNewConfig}
+  class="cursor-pointer flex w-full items-center"
+>
+  <div
+    class="hover:border-pick hover:bg-select-saturate-10 border-secondary
+              transition-colors duration-300 w-full border-l-4 text-white pl-4 p-2"
+  >
+    Add action block...
+  </div>
+</action-placeholder>
+
+{#if showActionPicker}
+  <ActionPicker {index} {referenceElement} on:close={handleCloseActionPicker} />
+{/if}
