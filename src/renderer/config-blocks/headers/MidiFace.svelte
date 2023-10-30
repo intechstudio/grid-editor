12aea6f42b3b5aedd4c12cea578eda850034186a
elsoazemelet <elsoazemelet@gmail.com>
2023-10-30 16:11:42 +0100

    PR fix

--- src/renderer/config-blocks/headers/MidiFace.svelte
+++ src/renderer/config-blocks/headers/MidiFace.svelte
@@ -10,6 +10,7 @@
   export let config = undefined;
 
   let scriptSegments = ["", "", "", ""];
+  let labels = ["CH:", "CMD:", "P1:", "P2:"];
 
   $: handleConfigChange(config);
 
@@ -29,33 +30,26 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
 <div
-  class="{$$props.class} text-white flex items-center flex-row w-full gap-4 pl-2"
+  class="{$$props.class} text-white flex items-center flex-row w-full"
   on:click={handleClick}
 >
-  <div class="flex flex-row items-center gap-2">
-    <span>Command:</span>
+  <div class="flex flex-grow h-full pr-4">
     <div
-      class="bg-primary p-1 my-auto rounded flex items-center w-10 h-full"
-      on:click|stopPropagation
+      class="grid grid-cols-[auto_1fr_auto_1fr_auto_1fr_auto_1fr] grid-flow-col items-center flex-grow"
     >
-      <LineEditor
-        bind:value={scriptSegments[1]}
-        {access_tree}
-        disabled={true}
-      />
-    </div>
-  </div>
-  <div class="flex flex-row items-center gap-2">
-    <span>Value:</span>
-    <div
-      class="bg-primary p-1 my-auto rounded flex items-center w-10 h-full"
-      on:click|stopPropagation
-    >
-      <LineEditor
-        bind:value={scriptSegments[2]}
-        {access_tree}
-        disabled={true}
-      />
+      {#each Array(scriptSegments.length).keys() as i}
+        <span class="text-center truncate p-2">{labels[i]}</span>
+        <div
+          class="bg-primary p-1 my-auto rounded items-center"
+          on:click|stopPropagation
+        >
+          <LineEditor
+            bind:value={scriptSegments[i]}
+            {access_tree}
+            disabled={true}
+          />
+        </div>
+      {/each}
     </div>
   </div>
   <div class="ml-auto self-start grid grid-cols-[auto_1fr] -mr-[1px] -mt-[1px]">
@@ -68,8 +62,7 @@
 
     <span
       style="background: {config.information.color};"
-      class="pr-2 pl-1 italic text-sm truncate"
-      >{config.information.blockTitle}</span
+      class="pr-2 pl-1 italic text-sm">{config.information.blockTitle}</span
     >
   </div>
 </div>
