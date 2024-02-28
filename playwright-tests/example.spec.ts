69bd008e550e4ddff83d7f5035b93a7d6b7855ba
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2024-02-28 14:40:51 +0100

    KKERTI revert to i instead of ci

--- playwright-tests/example.spec.ts
+++ playwright-tests/example.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
