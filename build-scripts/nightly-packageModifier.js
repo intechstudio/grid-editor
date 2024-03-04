89e496383b323e7daad249d2f8ddb5141cc54114
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2024-03-04 12:37:06 +0100

    KKERTI fix run order

--- build-scripts/nightly-packageModifier.js
+++ build-scripts/nightly-packageModifier.js
@@ -8,31 +8,7 @@ let packageJson = require(packageJsonPath);
 
 packageJson.version += `-nightly`;
 
-// fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
-//   if (err) throw err;
-//   console.log("package.json has been updated!");
-// });
-
-let branchName = process.env.BRANCH_NAME;
-console.log("branchName: ", branchName);
-
-// Update product name in electron-builder.json
-
-let electronBuilderConfigPath = path.join(__dirname, "../electron-builder.js");
-
-let electronBuilderConfig = require(electronBuilderConfigPath);
-
-electronBuilderConfig(` (Nightly) ${branchName}`);
-
-// electronBuilderConfig.config.productName += ` (Nightly) ${branchName}`;
-
-console.log("electronBuilderConfig: ", electronBuilderConfig());
-
-// fs.writeFile(
-//   electronBuilderConfigPath,
-//   JSON.stringify(electronBuilderConfig, null, 2),
-//   (err) => {
-//     if (err) throw err;
-//     console.log("electron-builder.js has been updated!");
-//   }
-// );
+fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+  if (err) throw err;
+  console.log("package.json has been updated!");
+});
