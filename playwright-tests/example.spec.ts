edbfd59d5e2537fcd0ec6d01fb3027d9c9f50abb
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2024-04-29 14:13:32 +0200

    kk playwright ci/cd

--- playwright-tests/example.spec.ts
+++ playwright-tests/example.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from "@playwright/test";
+import { dirname } from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  console.log(process.env.CI, "whatever: ", process.env.WHATEVER);
+
+  await page.goto(`file://${process.cwd()}/dist-web/index.html`);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Editor/);
 });
