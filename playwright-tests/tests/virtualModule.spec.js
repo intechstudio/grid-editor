c95e35f96908035b8ba13ba8cca306c40140a236
gergelybartha <gergely.bartha95@gmail.com>
2024-06-26 13:24:24 +0200

    test pass and interact

--- playwright-tests/tests/virtualModule.spec.js
+++ playwright-tests/tests/virtualModule.spec.js
@@ -5,17 +5,17 @@ import { PAGE_PATH } from "../utility";
 test("add bu", async ({ page }) => {
   const VirtualModule = new VirtualModulePage(page);
 
-  await page.goto("https://grid-editor-web.web.app/");
+  await page.goto(PAGE_PATH);
   // await page.goto("https://www.google.com");
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
   await page.screenshot({ path: "screenshotDIRECT.png", fullPage: true });
   // page.on("console", (msg) => console.log(msg.text()));
   // await expect(page.getByText("Connect your modules!")).toBeVisible();
 
   // await page.goto("https://grid-editor-web.web.app/");
-  // await expect(page.getByText("active changes")).toBeVisible();
-  // await expect(page.getByText("Connect your modules!")).toBeVisible();
-  // await VirtualModule.openVirtualModules();
-  // await VirtualModule.selectModule("BU16");
+  await expect(page.getByText("active changes")).toBeVisible();
+  await expect(page.getByText("Connect your modules!")).toBeVisible();
+  await VirtualModule.openVirtualModules();
+  await VirtualModule.selectModule("BU16");
   // await page.pause();
 });
