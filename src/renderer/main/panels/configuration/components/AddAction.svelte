1bd15131e8dea47c8d88d9bc86dda0f8f86614cf
elsoazemelet <elsoazemelet@gmail.com>
2024-07-08 12:14:48 +0200

    Fixed QA issue

--- src/renderer/main/panels/configuration/components/AddAction.svelte
+++ src/renderer/main/panels/configuration/components/AddAction.svelte
@@ -35,18 +35,19 @@
   on:new-config={handleNewConfig}
   on:paste={handlePaste}
 >
-  <div
-    class="text-white/50 flex flex-row py-3 px-5 justify-between items-center gap-2"
+  <button
+    class="text-white/50 flex flex-row py-2 my-1 px-5 mr-4 justify-between items-center gap-2 group hover:bg-white/10"
     in:fade={{ delay: 200 }}
+    on:click={handleShowActionPicker}
   >
-    <span>{text}</span>
-    <MoltenPushButton style={"outlined"} click={handleShowActionPicker}>
-      <div slot="content" class="flex flex-row items-center gap-2">
+    <span class="text-start">{text}</span>
+    <div class="rounded px-2 py-1 border border-pick group-hover:bg-pick/40">
+      <div class="flex flex-row items-center gap-2 text-white">
         <span> Add </span>
         <span class="text-2xl">+</span>
       </div>
-    </MoltenPushButton>
-  </div>
+    </div>
+  </button>
 </container>
 
 {#if showActionPicker}
