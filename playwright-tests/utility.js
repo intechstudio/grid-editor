2e7980ccc2799674eef3bfabf66d951f3d5fa4df
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2024-07-22 13:51:08 +0200

    kk change to server test only

--- playwright-tests/utility.js
+++ playwright-tests/utility.js
@@ -1,4 +1,5 @@
 export const PAGE_PATH =
   process.env.CI == "true"
-    ? `file://${process.cwd()}/dist-web/index.html`
+    ? 'http://localhost:5173'
+    // ? `file://${process.cwd()}/dist-web/index.html`
     : `http://localhost:5173`;
