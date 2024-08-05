3709d9ab97fe4fd5ef860871da4f99f1124110f2
elsoazemelet <elsoazemelet@gmail.com>
2024-08-05 14:09:09 +0200

    Fixed UI-kit package version and tests

--- playwright-tests/pages/configPage.js
+++ playwright-tests/pages/configPage.js
@@ -39,7 +39,7 @@ export class ConfigPage {
     );
 
     this.elementEvent = {
-      Init: page.getByLabel("Init"),
+      Init: page.getByLabel("Setup"),
       Timer: page.getByLabel("Timer"),
       Utility: page.getByLabel("Utility"),
       "Midi rx": page.getByLabel("Midi rx"),
@@ -49,7 +49,7 @@ export class ConfigPage {
       Encoder: page.getByLabel("Encoder"),
     };
 
-    this.elementInitButton = page.getByLabel("Init");
+    this.elementInitButton = page.getByLabel("Setup");
     this.elementButtonButton = page.getByLabel("Button");
     this.elementEndlessButton = page.getByLabel("Endless");
     this.elementTimerButton = page.getByLabel("Timer");
