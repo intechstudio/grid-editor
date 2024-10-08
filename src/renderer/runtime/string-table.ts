c4ab163a16c1243f32e5953272c32278d8f7f091
elsoazemelet <elsoazemelet@gmail.com>
2024-10-08 16:45:43 +0200

    Missing files

---
+++ src/renderer/runtime/string-table.ts
@@ -0,0 +1,7 @@
+export namespace Runtime {
+  export enum ErrorText {
+    LENGTH_ERROR = `Modifications can not be synced with grid, 
+        maximum character limit reached. Shorten your code or delete action blocks.`,
+    SYNTAX_ERROR = `Action(s) with syntax error(s) can not be merged!`,
+  }
+}
