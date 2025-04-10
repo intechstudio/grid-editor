c6a406d21feb207c758b9c339e5df14aa1c55a82
unknown <elsoazemelet@gmail.com>
2025-04-10 12:53:18 +0200

    Fixed QA issues

--- src/renderer/config-blocks/headers/ForLoopHeader.svelte
+++ src/renderer/config-blocks/headers/ForLoopHeader.svelte
@@ -37,34 +37,36 @@
 </script>
 
 <container
-  class="px-2 w-full rounded-tr-xl justify-center flex flex-col"
+  class="px-2 w-full rounded-tr-xl justify-center flex flex-col pointer-events-none"
   style="background-color:{config.information.color}"
 >
-  <div
-    class="flex flex-row flex-grow items-center gap-2 py-2 pointer-events-auto text-white"
-  >
+  <div class="flex flex-row flex-grow items-center gap-2 py-2 text-white">
     <span>Repeat</span>
-    <MeltCombo
-      bind:value={$data.variable.value}
-      suggestions={$data.variable.suggestions}
-      validator={$data.variable.validator.func}
-      on:input={(e) => {
-        const { value, validationError } = e.detail;
-        $data.variable.validator.value = !validationError;
-        sendData();
-      }}
-      on:change={() => dispatch("sync")}
-      postProcessor={GridScript.shortify}
-      preProcessor={GridScript.humanize}
-      valueInfoEnabled={false}
-    />
+    <div class="pointer-events-auto">
+      <MeltCombo
+        bind:value={$data.variable.value}
+        suggestions={$data.variable.suggestions}
+        validator={$data.variable.validator.func}
+        on:input={(e) => {
+          const { value, validationError } = e.detail;
+          $data.variable.validator.value = !validationError;
+          sendData();
+        }}
+        on:change={() => dispatch("sync")}
+        postProcessor={GridScript.shortify}
+        preProcessor={GridScript.humanize}
+        valueInfoEnabled={false}
+      />
+    </div>
     <span>for</span>
-    <MoltenInput
-      bind:target={$data.increment}
-      on:input={sendData}
-      on:change={() => dispatch("sync")}
-      availableCharacters={$event.getAvailableChars()}
-    />
+    <div class="pointer-events-auto">
+      <MoltenInput
+        bind:target={$data.increment}
+        on:input={sendData}
+        on:change={() => dispatch("sync")}
+        availableCharacters={$event.getAvailableChars()}
+      />
+    </div>
     <span class="mr-10">times</span>
   </div>
 </container>
