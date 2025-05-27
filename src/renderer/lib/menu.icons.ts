b0d32490e745d615b19ccf1b8e2c60e10fc5b869
Dániel <danim1130@gmail.com>
2025-05-27 16:21:23 +0200

    Export svg icons from navtabs to separate svg files

--- src/renderer/lib/menu.icons.ts
+++ src/renderer/lib/menu.icons.ts
@@ -3,6 +3,8 @@ import menu_debug_monitor from "../assets/menu/menu_debug_monitor.svg?raw";
 import menu_midi_monitor from "../assets/menu/menu_midi_monitor.svg?raw";
 import menu_websocket_monitor from "../assets/menu/menu_websocket_monitor.svg?raw";
 import menu_package_general from "../assets/menu/menu_package_general.svg?raw";
+import menu_preferences from "../assets/menu/menu_preferences.svg?raw";
+import menu_packages from "../assets/menu/menu_packages.svg?raw";
 
 type MenuIconMap = {
   [key: string]: string;
@@ -14,6 +16,8 @@ const menuIconMap: MenuIconMap = {
   menu_midi_monitor,
   menu_websocket_monitor,
   menu_package_general,
+  menu_preferences,
+  menu_packages,
 };
 
 export default menuIconMap;
