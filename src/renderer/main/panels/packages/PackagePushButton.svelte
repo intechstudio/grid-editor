80ab9f9de688d52f5a0008c9381ccb04dba34c89
Dániel <danim1130@gmail.com>
2025-07-01 01:44:04 +0200

    Format

--- src/renderer/main/panels/packages/PackagePushButton.svelte
+++ src/renderer/main/panels/packages/PackagePushButton.svelte
@@ -3,6 +3,9 @@
   export let text = "";
 </script>
 
-<button on:click={click} class="bg-emerald-600 px-1 hover:bg-emerald-700 rounded active:border-neutral-800 border-neutral-500 active:outline-none active:ring-blue-300 active:ring-2">
+<button
+  on:click={click}
+  class="bg-emerald-600 px-1 hover:bg-emerald-700 rounded active:border-neutral-800 border-neutral-500 active:outline-none active:ring-blue-300 active:ring-2"
+>
   <p class="text-white line-clamp-1">{text}</p>
 </button>
