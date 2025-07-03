1218388460db7ecb5dde738280baa38f2c52e35d
sukuwc <sukuwc@riseup.net>
2025-07-03 23:17:56 +0200

    SUKU element list filtered based on userLevel

--- src/renderer/main/panels/configuration/ElementSelectionPanel.svelte
+++ src/renderer/main/panels/configuration/ElementSelectionPanel.svelte
@@ -8,6 +8,7 @@
   } from "../../../runtime/moduleOverlay";
   import TooltipQuestion from "../../user-interface/tooltip/TooltipQuestion.svelte";
   import { user_input } from "./../../../runtime/user-input.store";
+  import { appSettings } from "./../../../runtime/app-helper.store";
   import { GridPage, PageData } from "../../../runtime/runtime";
 
   export let page: GridPage;
@@ -41,7 +42,7 @@
     });
   }
 
-  $: handlePageChange($page);
+  $: handlePageChange($page, $appSettings);
 
   function handlePageChange(page: PageData) {
     if (typeof page === "undefined") {
@@ -50,7 +51,11 @@
       return;
     }
 
-    const elements = page.control_elements;
+    const elements = page.control_elements.filter(
+      (e) =>
+        e.getHumanName().indexOf("System") === -1 ||
+        $appSettings.persistent.userLevel === "expert",
+    );
     options = elements.map((e) => {
       const stringName = e.name;
       if (typeof stringName !== "undefined") {
