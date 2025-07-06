5234d54c27cce9dc31163be13c111c8f36ee834e
suku <suku@2a01-036d-0106-2025-40cf-7c4e-1810-2a35.pool6.digikabel.hu>
2025-07-06 21:50:23 +0200

    SUKU tracker layout updated

--- src/renderer/main/user-interface/Tracker.svelte
+++ src/renderer/main/user-interface/Tracker.svelte
@@ -47,51 +47,52 @@
 </script>
 
 <container class={$$props.class}>
-  <div
-    class="flex flex-col items-end justify-end gap-2 bg-primary py-2 px-3 rounded-lg"
-  >
-    <div class="flex flex-row items-center gap-2">
-      <span class="text-gray-500 text-sm">Element Name Overlay</span>
+  <div class="flex flex-row items-center gap-3 bg-primary py-2 px-3 rounded-lg">
+    <div
+      class="flex gap-2 items-center"
+      use:tooltip={{
+        placement: "top",
+        class: "w-60 p-4 z-10",
+        key: "configuration_element_name",
+      }}
+    >
+      <span class="text-white">Name Overlay:</span>
 
       <Toggle
         on:change={showControlElementNameOverlay}
         toggleValue={$moduleOverlay === "control-name-overlay"}
       />
     </div>
-
-    <div class="flex flex-row">
-      <div class="flex flex-row gap-2 items-center">
-        <span class="text-white">Track:</span>
-        <div
-          use:tooltip={{
-            placement: "top",
-            class: "w-60 p-4 z-10",
-            key: "tracker_tooltip",
-          }}
-          class="w-24 h-fit text-white"
-        >
-          <MeltSelect
-            bind:target={$appSettings.persistent.changeOnEvent}
-            {options}
-          />
-        </div>
-      </div>
-      <div
-        use:tooltip={{
-          placement: "top",
-          class: "w-60 p-4 z-10",
-          key: "reset_grid_layout",
-        }}
-      >
-        <MoltenPushButton
-          text={"Reset View"}
-          click={handleGridLayoutResetClicked}
-          snap={"full"}
-          disabled={$appSettings.gridLayoutShift.x == 0 &&
-            $appSettings.gridLayoutShift.y == 0 &&
-            $appSettings.persistent.size == $appSettings.defaultSize}
-        />
-      </div>
+    <div
+      class="flex gap-2 items-center"
+      use:tooltip={{
+        placement: "top",
+        class: "w-60 p-4 z-10",
+        key: "tracker_tooltip",
+      }}
+    >
+      <span class="text-white">Track:</span>
+      <MeltSelect
+        bind:target={$appSettings.persistent.changeOnEvent}
+        {options}
+      />
     </div>
-  </div>
-</container>
+    <div
+      class="flex"
+      use:tooltip={{
+        placement: "top",
+        class: "w-60 p-4 z-10",
+        key: "reset_grid_layout",
+      }}
+    >
+      <MoltenPushButton
+        text={"Reset View"}
+        click={handleGridLayoutResetClicked}
+        snap={"full"}
+        disabled={$appSettings.gridLayoutShift.x == 0 &&
+          $appSettings.gridLayoutShift.y == 0 &&
+          $appSettings.persistent.size == $appSettings.defaultSize}
+      />
+    </div>
+  </div></container
+>
