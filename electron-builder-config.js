1127e877dab907c333b933a91b806c1bf8d6d885
sukuwc <sukuwc@riseup.net>
2025-11-19 21:58:57 +0100

    SUKU executableName test

--- electron-builder-config.js
+++ electron-builder-config.js
@@ -48,6 +48,7 @@ const config = {
         repo: "grid-editor",
       },
     ],
+    executableName: "grid-editor-nightly",
     artifactName: "${name}-windows-${version}-x64.${ext}",
     target: ["nsis"],
     icon: "build-assets/icon.png",
