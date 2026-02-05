92beabfa221e69bdeefd4e5b54e49058e38e00a5
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2026-02-05 16:25:58 +0100

    kk app starts without syntaxErrors

--- renderer.vite.config.mjs
+++ renderer.vite.config.mjs
@@ -7,6 +7,11 @@ export const rendererConfig = ({ outDir = "", additionalPlugins = [] }) => {
   return {
     plugins: [
       svelte({
+        compilerOptions: {
+          compatibility: {
+            componentApi: 4
+          }
+        },
         preprocess: [
           sveltePreprocess({postcss:true})
         ]
