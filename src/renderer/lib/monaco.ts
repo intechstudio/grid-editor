21f24aa5afb20a92ff497a49ae8eaed11bed4b0c
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2026-03-27 11:03:05 +0100

    kk fix monaco import, without the full import luals did not work

--- src/renderer/lib/monaco.ts
+++ src/renderer/lib/monaco.ts
@@ -7,7 +7,7 @@ import {
   languages as monaco_languages,
   Position,
   Range,
-} from "monaco-editor/esm/vs/editor/editor.api.js";
+} from "monaco-editor";
 import { TabFocus } from "monaco-editor/esm/vs/editor/browser/config/tabFocus.js";
 import { ElementType, grid } from "@intechstudio/grid-protocol";
 import { startLuaLSClient, stopLuaLSClient } from "./monaco-luals-client";
