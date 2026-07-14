5a3b12895244bbb5018bb64c4409404aeb3ac356
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2026-07-14 17:20:59 +0200

    kk basically an empty commit

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -973,7 +973,6 @@ ipcMain.handle("getLatestVideo", async (event, arg) => {
 
 // launch browser and open url
 ipcMain.handle("openInBrowser", async (event, arg) => {
-  console.log(arg.url);
   return await shell.openExternal(arg.url);
 });
 
